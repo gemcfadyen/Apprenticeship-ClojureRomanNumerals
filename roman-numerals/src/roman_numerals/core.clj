7d2e7ba7e3083597675b281abb18157c51e21f30
gemcfadyen <gemcfadyen@hotmail.co.uk>
2016-03-07 13:36:58 +0000

    Refactors the converts method to use a map

--- roman-numerals/src/roman_numerals/core.clj
+++ roman-numerals/src/roman_numerals/core.clj
@@ -1,9 +1,7 @@
 (ns roman-numerals.core)
 
+(def romanNumerals {1 "I" 5 "V" 10 "X"})
+
 (defn converts [arabic]
-  (cond
-    (= 1 arabic) "I"
-    (= 5 arabic) "V"
-    (= 10 arabic) "X"
-    )
-  )
+  (get romanNumerals arabic)
+)
