2986c51049ef9b3c56b82029a1b3223f70e6381f
gemcfadyen <gemcfadyen@hotmail.co.uk>
2016-03-07 13:42:40 +0000

    Converts 2 to II

--- roman-numerals/spec/roman_numerals/core_spec.clj
+++ roman-numerals/spec/roman_numerals/core_spec.clj
@@ -3,12 +3,19 @@
             [roman-numerals.core :refer :all]))
 
 (describe "Roman Numeral Converter"
-  (it "converts 1 to I"
-    (should= "I" (converts 1)))
+          (it "converts 1 to I"
+              (should= "I" (converts 1)))
 
-  (it "converts 5 to V"
-    (should= "V" (converts 5)))
+          (it "converts 5 to V"
+              (should= "V" (converts 5)))
 
-  (it "converts 10 to X"
-    (should= "X" (converts 10)))
-  )
+          (it "converts 10 to X"
+              (should= "X" (converts 10)))
+
+          (it "converts 1 to II"
+              (should= "II" (converts 2)))
+
+
+          )
+
+
--- roman-numerals/src/roman_numerals/core.clj
+++ roman-numerals/src/roman_numerals/core.clj
@@ -3,5 +3,5 @@
 (def romanNumerals {1 "I" 5 "V" 10 "X"})
 
 (defn converts [arabic]
-  (get romanNumerals arabic)
+  (if (contains? romanNumerals arabic) (get romanNumerals arabic) "II")
 )
