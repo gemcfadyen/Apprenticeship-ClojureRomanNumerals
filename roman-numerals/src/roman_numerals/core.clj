fe29eabf0bace9a1d7736ffff49026600d65dac4
gemcfadyen <gemcfadyen@hotmail.co.uk>
2016-03-07 17:17:08 +0000

    recursion used to drill out the algorithm

--- roman-numerals/src/roman_numerals/core.clj
+++ roman-numerals/src/roman_numerals/core.clj
@@ -1,14 +1,56 @@
 (ns roman-numerals.core)
 
-(def roman-numerals {10 "X" 5 "V" 1 "I"})
+(def arabics [10 5 1 ])
+(def romans ["X" "V" "I"])
+
+(defn- calculate [result arabic romans arabics]
+  (print "calculating with ")
+  (print result)
+  (print arabic)
+
+  (if (= 0 arabic)
+    result
+
+    ;; first each entry in arabics, find entry >= 'arabic'
+    ;; append the romans equiv to result
+    ;; deduct arabic entry found in step 1 from 'arabic'
+    ;; call calculate again result+roman, arabic - val, romans arabics
+    (if (>= arabic (first arabics))
+      (calculate (str result (first romans)) (- arabic (first arabics)) romans arabics)
+      (calculate result arabic (rest romans) (rest arabics)))
+
+    ;; else
+    ;; skip onto the next entry in arabics
+    ;; call calculate result arabic tail-of romans, tail-of arabics
+
+
+    ;;(calculate (conj result "W") (- (decimal 1)))
+    ))
+
 
 (defn converts [arabic]
-  (apply str (map
-   (fn [a]
-    (if (>= arabic (key a))
-      (val a)
-      ""
-      )) roman-numerals))
+  (calculate "" arabic romans arabics)
+  )
+
+
+(defn xcalculate [tuple roman-result input]
+  (if (>= input (key tuple))
+    (conj roman-result (val tuple))
+    (conj roman-result "")
+    )
+  )
+
+(defn xconverts [arabic]
+  (reduce
+    (fn [roman-results decimal-val]
+      (map
+        (fn [a]
+          (if (>= decimal-val (key a))
+            (conj roman-results (val a))
+            (conj roman-results "")
+            ))) ) [""] [arabic])
+
+
 
   ;;  (cond
   ;;    (contains? romanNumerals arabic) (get romanNumerals arabic)
@@ -17,3 +59,5 @@
   ;;    (= arabic 6) "VI"
   ;;   )
   )
+
+
