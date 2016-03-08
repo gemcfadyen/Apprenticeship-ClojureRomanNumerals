2c7fa0f35fb1c9d65e2fc91c094065f8ea4e57f5
gemcfadyen <gemcfadyen@hotmail.co.uk>
2016-03-08 11:20:18 +0000

    Small refactorings after IPM

--- roman-numerals/spec/roman_numerals/core_spec.clj
+++ roman-numerals/spec/roman_numerals/core_spec.clj
@@ -3,48 +3,48 @@
             [roman-numerals.core :refer :all]))
 
 (describe "Roman Numeral Converter"
-          (it "converts 0 to empty string"
-              (should= "" (converts 0)))
+          (it "convert 0 to empty string"
+              (should= "" (convert 0)))
 
-          (it "converts 1 to I"
-              (should= "I" (converts 1)))
+          (it "convert 1 to I"
+              (should= "I" (convert 1)))
 
-          (it "converts 2 to II"
-              (should= "II" (converts 2)))
+          (it "convert 2 to II"
+              (should= "II" (convert 2)))
 
-          (it "converts 3 to III"
-              (should= "III" (converts 3)))
+          (it "convert 3 to III"
+              (should= "III" (convert 3)))
 
-          (it "converts 4 to IV"
-              (should= "IV" (converts 4)))
+          (it "convert 4 to IV"
+              (should= "IV" (convert 4)))
 
-          (it "converts 5 to V"
-              (should= "V" (converts 5)))
+          (it "convert 5 to V"
+              (should= "V" (convert 5)))
 
-          (it "converts 6 to VI"
-              (should= "VI" (converts 6)))
+          (it "convert 6 to VI"
+              (should= "VI" (convert 6)))
 
-          (it "converts 9 to IX"
-              (should= "IX" (converts 9)))
+          (it "convert 9 to IX"
+              (should= "IX" (convert 9)))
 
-          (it "converts 10 to X"
-              (should= "X" (converts 10)))
+          (it "convert 10 to X"
+              (should= "X" (convert 10)))
 
-          (it "converts 50 to L"
-              (should= "L" (converts 50)))
+          (it "convert 50 to L"
+              (should= "L" (convert 50)))
 
-          (it "converts 90 to XC"
-              (should= "XC" (converts 90)))
+          (it "convert 90 to XC"
+              (should= "XC" (convert 90)))
 
-          (it "converts 100 to C"
-              (should= "C" (converts 100)))
+          (it "convert 100 to C"
+              (should= "C" (convert 100)))
 
-          (it "converts 900 to CM"
-              (should= "CM" (converts 900)))
+          (it "convert 900 to CM"
+              (should= "CM" (convert 900)))
 
-          (it "converts 1000 to M"
-              (should= "M" (converts 1000)))
+          (it "convert 1000 to M"
+              (should= "M" (convert 1000)))
 
-          (it "converts 3999 to MMMCMXCIX"
-              (should= "MMMCMXCIX" (converts 3999)))
+          (it "convert 3999 to MMMCMXCIX"
+              (should= "MMMCMXCIX" (convert 3999)))
           )
--- roman-numerals/src/roman_numerals/core.clj
+++ roman-numerals/src/roman_numerals/core.clj
@@ -2,27 +2,13 @@
 (def arabics [1000 900 100 90 50 10 9 5 4 1])
 (def romans ["M" "CM" "C" "XC" "L" "X" "IX" "V" "IV" "I"])
 
-(defn- join [result item]
-  (str result item))
-
 (defn- calculate [result arabic romans arabics]
   (if (= 0 arabic)
     result
 
-    ;; else
-    ;; check the first entry in arabics
-    ;; append the romans equiv to result
-    ;; deduct arabic entry found in step 1 from 'arabic'
-    ;; call calculate again result+roman, arabic - val, romans arabics
-
-    ;; else
-    ;; skip onto the next entry in arabics
-    ;; call calculate result arabic tail-of romans, tail-of arabics
-
     (if (>= arabic (first arabics))
-      (calculate (join result (first romans)) (- arabic (first arabics)) romans arabics)
+      (calculate (str result (first romans)) (- arabic (first arabics)) romans arabics)
       (calculate result arabic (rest romans) (rest arabics)))))
 
-
-(defn converts [arabic]
+(defn convert [arabic]
   (calculate "" arabic romans arabics))
